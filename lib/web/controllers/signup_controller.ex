72342f629a2b824d7de7224c85a15159c15f9b01
jjl <james.laver@gmail.com>
2020-11-10 09:25:30 +0000

    signup passes current_account to template

--- lib/web/controllers/signup_controller.ex
+++ lib/web/controllers/signup_controller.ex
@@ -2,12 +2,10 @@ defmodule VoxPublica.Web.SignupController do
   use VoxPublica.Web, :controller
   alias VoxPublica.Accounts
 
-  plug MustBeGuest
-
   def index(conn, _) do
     if get_session(conn, :account_id),
-      do: redirect(conn, to: "/home"),
-      else: render(conn, "form.html", registered: false, error: nil, form: form())
+      do: redirect(conn, to: "/_"),
+      else: render(conn, "form.html", current_account: nil, registered: false, error: nil, form: form())
   end
 
   def create(conn, params) do
@@ -16,11 +14,11 @@ defmodule VoxPublica.Web.SignupController do
     else
       case Accounts.signup(Map.get(params, "signup_form", %{})) do
         {:ok, _account} ->
-          render(conn, "form.html", registered: true)
+          render(conn, "form.html", current_account: nil, registered: true)
         {:error, :taken} ->
-          render(conn, "form.html", registered: false, error: :taken, form: form())
+          render(conn, "form.html", current_account: nil, registered: false, error: :taken, form: form())
         {:error, changeset} ->
-          render(conn, "form.html", registered: false, error: nil, form: changeset)
+          render(conn, "form.html", current_account: nil, registered: false, error: nil, form: changeset)
       end
     end
   end
